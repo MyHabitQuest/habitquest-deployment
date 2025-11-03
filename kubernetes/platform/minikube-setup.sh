36c043afc21a23c7b44acf2adfeabbd2e85678fa
MHS <muhamad.salman2001@gmail.com>
2025-11-03 16:06:40 +0100

    feat: updated keycloak chart images

--- kubernetes/platform/minikube-setup.sh
+++ kubernetes/platform/minikube-setup.sh
@@ -4,4 +4,4 @@ minikube start --cpus 2 --memory 4g --driver docker --profile crewcash
 echo -e "\n Enabling NGINX Ingress Controller...\n"
 minikube addons enable ingress --profile crewcash
 
-minikube tunnel --profile crewcash &
+# minikube tunnel --profile crewcash &
