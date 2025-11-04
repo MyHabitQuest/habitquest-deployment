ecc71e431b3ff7b20d91dc10093752f80c8e375b
MHS <muhamad.salman2001@gmail.com>
2025-11-04 13:19:54 +0100

    feat: added script to retrieve credentials and ips

--- kubernetes/credentials.sh
+++
@@ -1,5 +0,0 @@
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode
-
-# adjust namespace and secret name if different (here: keycloak-system and crewcash-keycloak)
-kubectl -n keycloak-system get secret crewcash-keycloak -o jsonpath="{.data.admin-user}" | base64 --decode
-kubectl -n keycloak-system get secret crewcash-keycloak -o jsonpath="{.data.admin-password}" | base64 --decode
